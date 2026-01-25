cc0d1b9004630b0f1b50927dfb13e10a2e604b4d
Omar Anwar <anwrmr649@gmail.com>
2026-01-25 04:36:38 +0200

    increase security of logins and signups

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -5,12 +5,16 @@ import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 import { SignJWT } from "jose";
 import { eq } from "drizzle-orm";
+import bcrypt from "bcryptjs";
 
 export const createUser = async (formData: FormData) => {
   const name = formData.get('name') as string
   const username = formData.get('username') as string
   const password = formData.get('password') as string
-  db.insert(user).values({ name, username, password }).run()
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  await db.insert(user).values({ name, username, password: hashedPassword }).run()
   redirect('/log-in')
 }
 
@@ -19,12 +23,13 @@ export const loginUser = async (formData: FormData) => {
   const password = formData.get('password') as string
 
   const existingUser = await db.select().from(user).where(eq(user.username, username)).get()
-  if (!existingUser || existingUser.password !== password) {
-    throw new Error('Invalid credentials')
+  const res = await bcrypt.compare(password, existingUser?.password || '')
+  if (!existingUser || !res) {
+    return { error: 'Invalid username or password' }
   }
 
   const secret = new TextEncoder().encode(process.env.JWT_SECRET)
-  const refreshToken = await new SignJWT({ username, password })
+  const refreshToken = await new SignJWT({ id: existingUser.id, password: existingUser.password })
     .setProtectedHeader({ alg: "HS256", typ: "JWT" })
     .setIssuedAt()
     .setExpirationTime("24w")
@@ -36,8 +41,22 @@ export const loginUser = async (formData: FormData) => {
     .sign(secret)
 
   const cookieStore = await cookies()
-  cookieStore.set("refresh_token", refreshToken, { httpOnly: true, path: '/' })  
-  cookieStore.set("access_token", accessToken, { httpOnly: true, path: '/' })
+  cookieStore.set("refresh_token", refreshToken, 
+    { 
+      httpOnly: true, 
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7 * 24, // 24 weeks
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax'
+    })  
+  cookieStore.set("access_token", accessToken, 
+    { 
+      httpOnly: true, 
+      path: '/',
+      maxAge: 60 * 60, // 1 hour
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax'
+    })
 
   redirect('/')
 }
