d5ce2045593abed3a5c3bfe598d6d2881957c5b6
Omar Anwar <anwrmr649@gmail.com>
2026-01-31 17:57:00 +0200

    style posting page

--- src/app/post-message/page.tsx
+++ src/app/post-message/page.tsx
@@ -5,14 +5,19 @@ import { useActionState } from "react";
 function MessagePostingPage() {
   const [state, action, pending] = useActionState(postMessage, undefined)
   return (
-    <>
-      <h1>Post a Message</h1>
-      <form action={action}>
-        <textarea name="message" placeholder="What's happening?" 
-        className="block"/>
-        <button type="submit" disabled={pending}>Post</button>
-      </form>
-    </>
+    <div className="flex justify-center items-center h-svh flex-col">
+      <h1 className="mb-4 font-bold text-2xl">Post a Message</h1>
+      <div className="container">
+        <form action={action}>
+          <input type="text" name="message" placeholder="What's happening?" autoFocus
+          className="block border border-foreground rounded-lg p-1 w-full"/>
+          {state?.message && 
+          (<p className="text-error">{state.message}</p>)}
+          <button type="submit" disabled={pending}
+          className="bg-foreground text-background p-2 rounded-lg w-full mt-3">Post</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
