0389e041ba85c378b4a084c760738363ccdf93c9
Omar Anwar <anwrmr649@gmail.com>
2026-01-31 16:20:05 +0200

    style home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,11 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div className="min-h-svh flex flex-col">
-      <header className="py-6 bg-blue-400 dark:bg-amber-800">
+      <header className="py-6 bg-foreground text-background">
         <h1 className="text-center text-4xl">Home Page</h1>
       </header>
       <nav className="h-full flex justify-center items-center flex-1">
-        <ul className="h-69 flex flex-col justify-between items-center 
-        bg-blue-400 dark:bg-amber-800 p-10 rounded-lg">
+        <ul className="h-69 flex flex-col justify-between items-center p-10 rounded-lg bg-foreground text-background">
           <li className="text-2xl"><Link href="/post-message">Post a message</Link></li>
           <li className="text-2xl"><Link href="/posts">See posts</Link></li>
         </ul>
