8942f3b00647d03961351bb580292a4953f869d6
Omar Anwar <anwrmr649@gmail.com>
2026-02-01 22:05:59 +0200

    fix styling mistake

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function HomePage() {
       <h1 className="text-center text-4xl font-bold mb-6">Home Page</h1>
       <nav className="h-full flex justify-center items-center">
         <ul className="flex flex-col gap-3 items-center p-10 rounded-lg">
-          <li className="text-2xl px-4 py-2 border border-foreground rounded-lg hover:bg-foreground hover:text-foreground"><Link href="/post-message">Post a message</Link></li>
-          <li className="text-2xl px-4 py-2 border border-foreground rounded-lg hover:bg-foreground hover:text-foreground"><Link href="/posts">See posts</Link></li>
+          <li className="text-2xl px-4 py-2 border border-foreground rounded-lg hover:bg-foreground hover:text-background"><Link href="/post-message">Post a message</Link></li>
+          <li className="text-2xl px-4 py-2 border border-foreground rounded-lg hover:bg-foreground hover:text-background"><Link href="/posts">See posts</Link></li>
         </ul>
       </nav>
     </div>
