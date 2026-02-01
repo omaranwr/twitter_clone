5bea83a1ac515caf9729649b6586d8dba102927f
Omar Anwar <anwrmr649@gmail.com>
2026-02-01 21:47:23 +0200

    style posts page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -9,18 +9,20 @@ async function PostsPage() {
   .orderBy(desc(post.timestamp)).all()
 
   return (
-    <>
-      <h1>Posts</h1>
-      <ul>
-        {res.map(item => (
-          <li key={item.post.id}>
-            <p>{item.user?.name}</p>
-            <p>{item.post.content}</p>
-            <p>{item.post.timestamp}</p>
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className="flex flex-col items-center pt-[20vh]">
+      <h1 className="title">Posts</h1>
+      <div className="wrapper">
+        <ul className="border border-foreground rounded-lg p-2 flex flex-col gap-5">
+          {res.map(item => (
+            <li key={item.post.id}>
+              <p className="opacity-85">{item.user!.name}</p>
+              <p className="text-xl">{item.post.content}</p>
+              <p className="text-xs opacity-75">{item.post.timestamp}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
 
